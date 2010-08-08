187a658517dc89724b0f1fe7a322c84209952a04
Bhaumik <bhaumik17@gmail.com>
2010-08-09 02:24:15 +0800

    add migration for groups_users table

---
+++ db/migrate/20100806164150_group_user.rb
@@ -0,0 +1,13 @@
+class GroupUser < ActiveRecord::Migration
+  def self.up
+     create_table :groups_users do |t|
+      t.integer :group_id
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+
+  def self.down
+     drop_table :groups_users
+  end
+end
