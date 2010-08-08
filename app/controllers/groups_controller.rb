f928582542a13b7636d79c48765b9ac04529be84
Bhaumik <bhaumik17@gmail.com>
2010-08-09 02:28:26 +0800

    do change in the group and users controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,6 @@
 class GroupsController < ApplicationController
-  before_filter :authenticate, :except => [:show, :new, :create]
-  # before_filter :correct_user, :only => [:edit, :update]
-  # before_filter :admin_user,   :only => :destroy
-
+  before_filter :authenticate, :except => [:all_groups]
+  
   def index
     @group = Group.find(:all,:conditions => "user_id = #{current_user.id} ")
     if @group.blank?
@@ -20,9 +18,14 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(params[:group])
+	@user_group = GroupsUser.new
+	
     @group.user_id = current_user.id
     if @group.save
-      # redirect_to @group
+	  @user_group.group_id = @group.id
+	  @user_group.user_id = current_user.id
+	  @user_group.save
+      flash[:notice] = "Group Create Successfully"
       redirect_to groups_path
     else
       #render :action => "new"
@@ -32,14 +35,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    #params[:data] = controller.action_name
-    #exit
     @group = Group.find(:first, :conditions => "id = #{params[:id]}")
   end
 
   def update
     @group = Group.find(:first, :conditions => "id = #{params[:id]}")
     if @group.update_attributes(params[:group])
+	  flash[:notice] = "Group Update successfully" 
       redirect_to groups_path
     else
     
@@ -49,12 +51,34 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(:first, :conditions => "id = #{params[:id]}")
-    @group.destroy
+    @user_group = GroupsUser.find(:first,:conditions=>["group_id=? and user_id=?",params[:id],current_user.id])
+	if @user_group.group.user_id == current_user.id
+		@group.destroy
+	end
+    @user_group
+    flash[:notice] = "Group Delete Successfully"
     redirect_to groups_path
   end
 
-  def invite
+    
+  def join_group
+	@group = GroupsUser.find(:first,:conditions=>["user_id=? and group_id=?",current_user.id,params[:id].to_i])
+	if @group
+		flash[:notice] = "You have already Joined This Group.."
+		redirect_to groups_path
+	else
+		    flash[:notice] = "New Group is added"
+    @group_user = GroupsUser.new
+    @group_user.group_id = params[:id]
+    @group_user.user_id = current_user.id
+    @group_user.save
+    redirect_to groups_path
+	end
 
+  end
+  
+  def invite
+  
   end
 
 
