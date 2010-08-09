93e1a801ac3ed9cf25b3bd6b14de00bde324483c
wolfgangihloff <wolfgang.ihloff@gmail.com>
2010-08-09 16:04:47 +0200

    updated .gitignore for .bundle dir

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100717111902) do
+ActiveRecord::Schema.define(:version => 20100806164150) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups_users", :force => true do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
