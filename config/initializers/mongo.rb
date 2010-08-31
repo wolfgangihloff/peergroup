2ce4ee81f84beeada21368fbce8e6147a7a2b1e6
kazjote <kazjote@ragnarson.com>
2010-08-31 13:08:49 +0200

    Configured mongo adapter to use mongodb.yml file

---
+++ config/initializers/mongo.rb
@@ -0,0 +1,10 @@
+# Read in the config info 
+mongo_cfg = YAML.load( IO.read("#{RAILS_ROOT}/config/mongodb.yml") )[RAILS_ENV]
+
+# Create the connection from mongodb.yml 
+conn_args = [mongo_cfg['host'],mongo_cfg['port']].compact
+MongoMapper.connection = Mongo::Connection.new(*conn_args)
+
+# Pick the Mongo database 
+db_cfg = Rails.configuration.database_configuration[RAILS_ENV]
+MongoMapper.database = mongo_cfg['database'] || db_cfg['database']
