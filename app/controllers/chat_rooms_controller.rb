d304580a09c705ebb7983c40926a1bc415e55e35
kazjote <kazjote@ragnarson.com>
2010-09-01 14:29:37 +0200

    Users beeing on the chat are shown while entering the chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,6 +2,7 @@ class ChatRoomsController < ApplicationController
 
   def index
     @chat_updates = ChatUpdate.all
+    current_user.seen_on_chat!
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100806164150) do
+ActiveRecord::Schema.define(:version => 20100901120451) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20100806164150) do
     t.string   "salt"
     t.string   "remember_token"
     t.boolean  "admin",              :default => false
+    t.time     "seen_on_chat"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
