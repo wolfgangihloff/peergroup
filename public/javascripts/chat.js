612716a1f736c64bcc9d2cc7a9439a0b3a37a605
kazjote <kazjote@ragnarson.com>
2010-09-01 19:40:51 +0800

    Visual changes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,18 +6,30 @@ jQuery(document).ready(function($) {
       var lastUpdate = $('.timestamp', container).text();
       var chatUpdateUrl = $('form.new_chat_update', container).attr('action');
 
+      document.location = '#chat_end';
+
       function updateChat() {
         $.getJSON(chatUpdateUrl, {last_update: lastUpdate}, function(data) {
           $.each(data.feeds, function(i, feed) {
             if(document.getElementById(feed.id) == null) {
               $(".chat_updates", container).append(feed.update);
+              $('a.chat_end', this).remove();
+              $(".chat_updates", container).append("<a name='chat_end' class='chat_end'></a>");
+              document.location = '#chat_end';
             };
           });
 
           lastUpdate = data.timestamp;
           setTimeout(updateChat, 1000);
+
         });
       };
+
+      $('form.new_chat_update', this).ajaxForm(function() {
+        $('#chat_update_message').val('');
+      });
+
+      $('#chat_update_message').focus();
 
       setTimeout(updateChat, 1000);
     });
