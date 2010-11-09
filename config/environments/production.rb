0675117bccbfac9322d353133adc345fa3126ba1
kazjote <kazjote@ragnarson.com>
2010-11-09 14:54:11 +0100

    Fixed production configuraiton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Peergroupsupervision::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -43,6 +43,8 @@ Peergroupsupervision::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+
+  I18n.default_locale = ENV['MONGOHQ_URL'] ? :de : :en
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
