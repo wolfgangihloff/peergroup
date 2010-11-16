d345c3c0fcb14566766709f2dc0ae4aa5804dffa
kazjote <kazjote@ragnarson.com>
2010-11-16 14:04:14 +0100

    Removed generate directory

--- generate/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f1c2e7cb8048259627e4b96e2733811d89ed43a80ed2d30c0260b9ceea8a6ccb38cfb9aff7769e408ffeb5a1754f0ed20914f2a89ce9b87ad70bae092456c54e';
