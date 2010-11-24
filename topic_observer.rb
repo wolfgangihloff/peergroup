ad068daaf566a657c76543383ce0df6d45c5b47e
Mirosław Boruta <m.boruta@objectreload.com>
2010-11-24 22:50:06 +0100

    Cleanup ignored files, fix gitignore, add rvmrc

--- topic_observer.rb
+++
@@ -1,9 +0,0 @@
-class TopicVoteObserver < ActiveRecord::Observer
-  def after_create(vote)
-    supervision = vote.supervision
-    if supervision.state == "topic_vote" && vote.statement.is_a?(Topic) && supervision.all_topics?
-      supervision.next_step!
-    end
-  end
-end
-
