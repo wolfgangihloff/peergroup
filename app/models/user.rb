d734acccf3453acedbc7b92f339ac1007d8af584
Mirosław Boruta <m.boruta@objectreload.com>
2010-11-24 22:53:40 +0100

    Remove schema annotation from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-# Schema version: 20100707090206
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessor :passcode
