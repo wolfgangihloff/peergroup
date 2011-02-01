128c1740f98ab6a3017d20ab9bf1c25aabba4977
Mirosław Boruta <m.boruta@objectreload.com>
2011-02-01 23:38:19 +0100

    Add scrolling chat messages functionality

--- app/javascripts/chat_room.js
+++ app/javascripts/chat_room.js
@@ -2,9 +2,19 @@
     $.fn.chatRoom = function() {
         return this.each(function() {
             var $this = $(this),
+                $messagesPart = $this.find(".messages_part"),
                 $messages = $this.find(".messages");
 
             $messages.find("time").timeago();
+
+            var scrollMessages = function(fn) {
+                var currentScroll = $messagesPart.scrollTop();
+                var maximumScroll = $messages.height() - $messagesPart.height();
+                fn();
+                if (currentScroll === maximumScroll) {
+                    $messagesPart.scrollTop($messages.height() - $messagesPart.height());
+                }
+            };
 
             var onMessage = function(event, message) {
                 var id = message.id || -1,
@@ -18,7 +28,10 @@
                     append($("<time>", { datetime: date.toISOString() }).append(date).timeago()).
                     append(" : ").
                     append($("<span>", { "class": "message-content", text: content }));
-                $messages.append(newMessage);
+
+                scrollMessages(function(){
+                    $messages.append(newMessage);
+                });
             };
             var onSystemMessage = function(event, message) {
                 var text = message.text || "",
@@ -28,7 +41,10 @@
                     append($("<time>", { datetime: date.toISOString() }).append(date).timeago()).
                     append(" ").
                     append($("<span>", { "class": "message-content", text: text }));
-                $messages.append(newMessage);
+
+                scrollMessages(function(){
+                    $messages.append(newMessage);
+                });
             };
 
             $this.bind({
