ca48636cb941f877bdb2355ba2b58dfdff85464c
Mirosław Boruta <m.boruta@objectreload.com>
2011-02-02 17:40:38 +0100

    Modify chat scrolling

--- app/javascripts/components/chat_room.js
+++ app/javascripts/components/chat_room.js
@@ -2,17 +2,25 @@
     $.fn.chatRoom = function() {
         return this.each(function() {
             var $this = $(this),
-                $messagesPart = $this.find(".messages_part"),
-                $messages = $this.find(".messages");
+                $messages = $this.find(".messages"),
+                $messagesParent = $messages.parent();
 
             $messages.find("time").timeago();
 
-            var scrollMessages = function(fn) {
-                var currentScroll = $messagesPart.scrollTop();
-                var maximumScroll = $messages.height() - $messagesPart.height();
-                fn();
-                if (currentScroll === maximumScroll) {
-                    $messagesPart.scrollTop($messages.height() - $messagesPart.height());
+
+            var scrollMessages = function() {
+                var newScrollTop = $messages.height() - $messagesParent.height();
+                $messagesParent.scrollTop(newScrollTop);
+            };
+            var addMessage = function(message) {
+                var shouldScroll =
+                    ($messagesParent.scrollTop() === $messages.height() - $messagesParent.height()) || // if messages are already scrolled
+                    ($messagesParent.height() > $messages.height()); // or if there are less messages than possible to display
+
+                $messages.append(message);
+
+                if (shouldScroll) {
+                    scrollMessages();
                 }
             };
 
@@ -22,31 +30,28 @@
                     date = message.date || new Date(),
                     content = message.content || "";
 
-                var newMessage = $("<li>", { "class": "chat_message", id: id }).
-                    append($("<span>", { "class": "message-user", text: user })).
-                    append(" ").
-                    append($("<time>", { datetime: date.toISOString() }).append(date).timeago()).
-                    append(" : ").
-                    append($("<span>", { "class": "message-content", text: content }));
+                var newMessage = $("<li>", { "class": "chat_message", id: id })
+                    .append($("<span>", { "class": "message-user", text: user }))
+                    .append(" ")
+                    .append($("<time>", { datetime: date.toISOString() }).append(date).timeago())
+                    .append(" : ")
+                    .append($("<span>", { "class": "message-content", text: content }));
 
-                scrollMessages(function(){
-                    $messages.append(newMessage);
-                });
+                addMessage(newMessage);
             };
             var onSystemMessage = function(event, message) {
                 var text = message.text || "",
                     date = message.date || new Date();
 
-                var newMessage = $("<li>", { "class": "system_chat_message" }).
-                    append($("<time>", { datetime: date.toISOString() }).append(date).timeago()).
-                    append(" ").
-                    append($("<span>", { "class": "message-content", text: text }));
+                var newMessage = $("<li>", { "class": "system_chat_message" })
+                    .append($("<time>", { datetime: date.toISOString() }).append(date).timeago())
+                    .append(" ")
+                    .append($("<span>", { "class": "message-content", text: text }));
 
-                scrollMessages(function(){
-                    $messages.append(newMessage);
-                });
+                addMessage(newMessage);
             };
 
+            scrollMessages();
             $this.bind({
                 "message": onMessage,
                 "systemMessage": onSystemMessage
