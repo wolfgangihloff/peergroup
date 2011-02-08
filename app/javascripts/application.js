2e31da439c2da72f070dbb41ad24a8f8a31a66e8
Mirosław Boruta <m.boruta@objectreload.com>
2011-02-08 15:39:15 +0100

    Fix problem with using socket.io
     * Load socket.io only when needed
     * Setup flashsocket location before loading socket.io

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -20,13 +20,24 @@ jQuery.timeago.settings.allowFuture = true;
 //= require "components/flash_notifications"
 
 jQuery(function($) {
+    // Add socket.io external javascript, use:
+    //     PGS.load("socket.io", function() {
+    //         ... // socket.io is loaded
+    //     });
+    (function(){
+        var nodeServerUrl = document.location.protocol + "//" + document.location.hostname + ":8080";
+        var socketIoUrl = nodeServerUrl + "/socket.io/socket.io.js";
+        // copied from app/node.js/socket.io/support/socket.io-client/socket.io.js
+        window.WEB_SOCKET_SWF_LOCATION = nodeServerUrl + "/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf";
+        PGS.addModule("socket.io", socketIoUrl);
+    })();
 
     $(".supervision").each(function(i,el) {
         $supervision = $(this);
         $chatRoom = $supervision.find(".chat_room");
         $supervisionContent = $supervision.find(".supervision-content");
 
-        var $pusher = $("<div style=height:0;margin:0;padding:0>");
+        var $pusher = $("<div style='height:0;margin:0;padding:0'>");
         $chatRoom.before($pusher);
         var scrollCallback = function() {
             var y = $(this).scrollTop();
@@ -54,19 +65,19 @@ jQuery(function($) {
         });
 
         var onUserEnters = function(event, user) {
-            PGS.userInfo(user.id, function(userId, userData) {
+            PGS.withUserInfo(user.id, function(userId, userData) {
                 var username = userData.name;
                 $chatRoom.trigger("systemMessage", { text: "<" + username + "> enter room" });
             });
         };
         var onUserExits = function(event, user) {
-            PGS.userInfo(user.id, function(userId, userData) {
+            PGS.withUserInfo(user.id, function(userId, userData) {
                 var username = userData.name;
                 $chatRoom.trigger("systemMessage", { text: "<" + username + "> exit room" });
             });
         };
         var onNewMessage = function(event, message) {
-            PGS.userInfo(message.user, function(userId, userData) {
+            PGS.withUserInfo(message.user, function(userId, userData) {
                 var date = new Date((+message.timestamp) * 1000);
                 $chatRoom.trigger("message", _.extend(message, { date: date, user: userData.name }));
             });
@@ -80,30 +91,31 @@ jQuery(function($) {
         var chatRoomToken = $chatRoom.data("token");
         var chatRoomId = $chatRoom.attr("id").replace("chat_room_", "");
 
-        var s = S(PGS.getSocket(), "chat");
-        s.on("authentication", function(type, message) {
-            if (message.status === "OK") {
-                if (window.console && window.console.log) {
-                    console.log("chat: Authenticated");
+        PGS.withSocket("chat", function(s) {
+            s.on("authentication", function(type, message) {
+                if (message.status === "OK") {
+                    if (window.console && window.console.log) {
+                        console.log("chat: Authenticated");
+                    }
+                } else {
+                    if (window.console && window.console.error) {
+                        console.error(message);
+                    }
                 }
-            } else {
-                if (window.console && window.console.error) {
-                    console.error(message);
+            });
+            s.on("presence", function(type, message) {
+                if (message.action === "enter") {
+                    $chatRoom.trigger("userEnters", { id: message.user });
+                } else if (message.action === "exit") {
+                    $chatRoom.trigger("userExits", { id: message.user });
                 }
-            }
-        });
-        s.on("presence", function(type, message) {
-            if (message.action === "enter") {
-                $chatRoom.trigger("userEnters", { id: message.user });
-            } else if (message.action === "exit") {
-                $chatRoom.trigger("userExits", { id: message.user });
-            }
-        });
-        s.on("message", function(type, message) {
-            $chatRoom.trigger("newMessage", message);
-        });
-        s.onConnect(function() {
-            this.send("authenticate", { userId: document.pgs.currentUser, token: chatRoomToken, chatRoom: chatRoomId });
+            });
+            s.on("message", function(type, message) {
+                $chatRoom.trigger("newMessage", message);
+            });
+            s.onConnect(function() {
+                this.send("authenticate", { userId: document.pgs.currentUser, token: chatRoomToken, chatRoom: chatRoomId });
+            });
         });
     });
 
@@ -114,26 +126,27 @@ jQuery(function($) {
         var supervisionToken = $supervision.data("token");
         var supervisionId = $supervision.attr("id").replace("supervision_", "");
 
-        var s = S(PGS.getSocket(), "supervision");
-        s.on("authentication", function(type, message) {
-            if (message.status === "OK") {
-                if (window.console && window.console.log) {
-                    console.log("supervision: Authenticated");
+        PGS.withSocket("supervision", function(s) {
+            s.on("authentication", function(type, message) {
+                if (message.status === "OK") {
+                    if (window.console && window.console.log) {
+                        console.log("supervision: Authenticated");
+                    }
+                } else {
+                    if (window.console && window.console.error) {
+                        console.error(message);
+                    }
                 }
-            } else {
-                if (window.console && window.console.error) {
-                    console.error(message);
-                }
-            }
-        });
-        s.onConnect(function() {
-            this.send("authenticate", { userId: document.pgs.currentUser, token: supervisionToken, supervision: supervisionId });
-        });
-        s.on("supervision", function(type, message) {
-            $supervision.trigger("supervisionUpdate", message.supervision);
-        });
-        s.on("topic", function(type, message) {
-            $supervision.trigger("newTopic", message.topic);
+            });
+            s.onConnect(function() {
+                this.send("authenticate", { userId: document.pgs.currentUser, token: supervisionToken, supervision: supervisionId });
+            });
+            s.on("supervision", function(type, message) {
+                $supervision.trigger("supervisionUpdate", message.supervision);
+            });
+            s.on("topic", function(type, message) {
+                $supervision.trigger("newTopic", message.topic);
+            });
         });
     });
 
@@ -144,44 +157,45 @@ jQuery(function($) {
         var supervisionToken = $supervision.data("token");
         var supervisionId = $supervision.attr("id").replace("supervision_", "");
 
-        var s = S(PGS.getSocket(), "supervision");
-        s.on("authentication", function(type, message) {
-            if (message.status === "OK") {
-                if (window.console && window.console.log) {
-                    console.log("supervision: Authenticated");
+        PGS.withSocket("supervision", function(s) {
+            s.on("authentication", function(type, message) {
+                if (message.status === "OK") {
+                    if (window.console && window.console.log) {
+                        console.log("supervision: Authenticated");
+                    }
+                } else {
+                    if (window.console && window.console.error) {
+                        console.error(message);
+                    }
                 }
-            } else {
-                if (window.console && window.console.error) {
-                    console.error(message);
-                }
-            }
-        });
-        s.onConnect(function() {
-            this.send("authenticate", { userId: document.pgs.currentUser, token: supervisionToken, supervision: supervisionId });
-        });
-        s.on("supervision", function(type, message) {
-            $supervision.trigger("supervisionUpdate", message.supervision);
-        });
-        s.on("question", function(type, message) {
-            $supervision.trigger("newQuestion", message.question);
-        });
-        s.on("answer", function(type, message) {
-            $supervision.trigger("newAnswer", message.answer);
-        });
-        s.on("idea", function(type, message) {
-            $supervision.trigger("newIdea", message.idea);
-        });
-        s.on("ideas_feedback", function(type, message) {
-            $supervision.trigger("newIdeasFeedback", message.ideas_feedback);
-        });
-        s.on("solution", function(type, message) {
-            $supervision.trigger("newSolution", message.solution);
-        });
-        s.on("solutions_feedback", function(type, message) {
-            $supervision.trigger("newSolutionsFeedback", message.solutions_feedback);
-        });
-        s.on("supervision_feedback", function(type, message) {
-            $supervision.trigger("newSupervisionFeedback", message.supervision_feedback);
+            });
+            s.onConnect(function() {
+                this.send("authenticate", { userId: document.pgs.currentUser, token: supervisionToken, supervision: supervisionId });
+            });
+            s.on("supervision", function(type, message) {
+                $supervision.trigger("supervisionUpdate", message.supervision);
+            });
+            s.on("question", function(type, message) {
+                $supervision.trigger("newQuestion", message.question);
+            });
+            s.on("answer", function(type, message) {
+                $supervision.trigger("newAnswer", message.answer);
+            });
+            s.on("idea", function(type, message) {
+                $supervision.trigger("newIdea", message.idea);
+            });
+            s.on("ideas_feedback", function(type, message) {
+                $supervision.trigger("newIdeasFeedback", message.ideas_feedback);
+            });
+            s.on("solution", function(type, message) {
+                $supervision.trigger("newSolution", message.solution);
+            });
+            s.on("solutions_feedback", function(type, message) {
+                $supervision.trigger("newSolutionsFeedback", message.solutions_feedback);
+            });
+            s.on("supervision_feedback", function(type, message) {
+                $supervision.trigger("newSupervisionFeedback", message.supervision_feedback);
+            });
         });
     });
 
--- app/javascripts/pgs.js
+++ app/javascripts/pgs.js
@@ -1,29 +1,41 @@
 (function() {
     var root = this;
     var usersCache = {};
-    var path = function(pathComponents, options) {
-        options = options || {};
-
-        var p = "/" + pathComponents.join("/");
-        if (options.format) {
-            p = p + "." + options.format;
-            delete options.format;
-        }
-        var params = [];
-        for (var k in options) {
-            if (Object.hasOwnProperty.call(options, k)) {
-                params.push([k, encodeURIComponent(options[k])].join("="));
-            }
-        }
-        if (params.length) {
-            p = p + "?" + params.join("&");
-        }
-        return p;
-    };
     var socket;
+    var modules = {};
 
     var PGS = {
-        userInfo: function(userId, callback) {
+
+        addModule: function(moduleName, url) {
+            if (!modules[moduleName]) {
+                modules[moduleName] = { loaded: false, url: url };
+            }
+        },
+
+        load: function(moduleName, callback) {
+            if (modules[moduleName].loaded) {
+                callback.call({});
+            } else {
+                $.ajax({
+                    url: modules[moduleName].url,
+                    dataType: "script",
+                    cache: true,
+                    success: function(){ callback.call({}); },
+                });
+            }
+        },
+
+        withSocket: function(namespace, callback) {
+            this.load("socket.io", function() {
+                if (typeof socket === "undefined") {
+                    socket = new io.Socket(null, { port: 8080 });
+                    socket.connect();
+                }
+                callback.call({}, S(socket, namespace));
+            });
+        },
+
+        withUserInfo: function(userId, callback) {
             if (usersCache[userId]) {
                 callback.call({}, userId, usersCache[userId]);
             } else {
@@ -35,52 +47,64 @@
             }
         },
                     
+        path: function(pathComponents, options) {
+            options = options || {};
+
+            var p = "/" + pathComponents.join("/");
+            if (options.format) {
+                p = p + "." + options.format;
+                delete options.format;
+            }
+            var params = [];
+            for (var k in options) {
+                if (Object.hasOwnProperty.call(options, k)) {
+                    params.push([k, encodeURIComponent(options[k])].join("="));
+                }
+            }
+            if (params.length) {
+                p = p + "?" + params.join("&");
+            }
+            return p;
+        },
+
         userPath: function(userId, options) {
-            return path(["users", userId], options);
+            return this.path(["users", userId], options);
         },
 
         supervisionPath: function(supervisionId, options) {
-            return path(["supervisions", supervisionId], options);
+            return this.path(["supervisions", supervisionId], options);
         },
 
         supervisionTopicsPath: function(supervisionId, options) {
-            return path(["supervisions", supervisionId, "topics"], options);
+            return this.path(["supervisions", supervisionId, "topics"], options);
         },
 
         supervisionTopicPath: function(supervisionId, topicId, options) {
-            return path(["supervisions", supervisionId, "topics", topicId], options);
+            return this.path(["supervisions", supervisionId, "topics", topicId], options);
         },
 
         supervisionQuestionPath: function(supervisionId, questionId, options) {
-            return path(["supervisions", supervisionId, "questions", questionId], options);
+            return this.path(["supervisions", supervisionId, "questions", questionId], options);
         },
 
         supervisionIdeaPath: function(supervisionId, ideaId, options) {
-            return path(["supervisions", supervisionId, "ideas", ideaId], options);
+            return this.path(["supervisions", supervisionId, "ideas", ideaId], options);
         },
 
         supervisionIdeasFeedbackPath: function(supervisionId, options) {
-            return path(["supervisions", supervisionId, "ideas_feedback"], options);
+            return this.path(["supervisions", supervisionId, "ideas_feedback"], options);
         },
 
         supervisionSolutionPath: function(supervisionId, solutionId, options) {
-            return path(["supervisions", supervisionId, "solutions", solutionId], options);
+            return this.path(["supervisions", supervisionId, "solutions", solutionId], options);
         },
 
         supervisionSolutionsFeedbackPath: function(supervisionId, options) {
-            return path(["supervisions", supervisionId, "solutions_feedback"], options);
+            return this.path(["supervisions", supervisionId, "solutions_feedback"], options);
         },
 
         supervisionSupervisionFeedbackPath: function(supervisionId, supervisionFeedbackId, options) {
-            return path(["supervisions", supervisionId, "supervision_feedbacks", supervisionFeedbackId], options);
-        },
-
-        getSocket: function() {
-            if (typeof socket === "undefined") {
-                socket = new io.Socket(null, { port: 8080 });
-                socket.connect();
-            }
-            return socket;
+            return this.path(["supervisions", supervisionId, "supervision_feedbacks", supervisionFeedbackId], options);
         }
 
     };
