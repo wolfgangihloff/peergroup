23b6ffdb984735a44afe5ef03399a6710296ee05
Mirosław Boruta <m.boruta@objectreload.com>
2011-03-16 16:04:54 +0100

    Fix wrong controller spec

--- spec/controllers/supervisions_controller_spec.rb
+++ spec/controllers/supervisions_controller_spec.rb
@@ -70,12 +70,12 @@ describe SupervisionsController do
     render_views
 
     before do
-      @chat_room = Factory.build(:chat_room, :id => 5)
-      @supervision = Factory(:supervision, :group => @group, :topic => Factory(:topic), :state => "asking_questions", :chat_room => @chat_room)
+      @supervision = Factory(:supervision, :group => @group, :topic => Factory(:topic), :state => "asking_questions")
+      @chat_room = @supervision.chat_room
       @user.join_supervision(@supervision)
 
       SecureRandom.should_receive(:hex).and_return("asdfb")
-      REDIS.should_receive(:setex).with("chat:#{@chat_room.id}:token:asdfb", 60, 1)
+      REDIS.should_receive(:setex).with("chat:#{@chat_room.id}:token:asdfb", 60, @user.id)
       REDIS.should_receive(:setex).with("supervision:#{@supervision.id}:users:#{@user.id}:token:asdfb", 60, "1")
 
       get :show, :id => @supervision.id
