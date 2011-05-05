23900462b287a014c386fa3f42f6da29fd870491
Wojciech Wnętrzak <w.wnetrzak@objectreload.com>
2011-05-05 16:07:09 +0200

    removed cucumber config files, features still left to have reference when writing integration specs

--- features/step_definitions/supervision_steps.rb
+++
@@ -1,5 +0,0 @@
-Given /^the session for the group "([^"]*)" is started by "([^"]*)"$/ do |group_name, founder|
-  group = Group.find_by_name(group_name)
-  group.supervisions.create!
-end
-
--- features/support/factory_girl.rb
+++
@@ -1,3 +0,0 @@
-require 'factory_girl'
-require File.join(Rails.root, 'spec', 'factories')
-
