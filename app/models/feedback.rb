f0e06d4c402428386c761d0f439b32b32efaa7ac
Wojciech Wnętrzak <w.wnetrzak@objectreload.com>
2011-05-25 16:34:11 +0200

    fixed typo in feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,7 +2,7 @@ class Feedback < ActiveRecord::Base
   belongs_to :user
   belongs_to :supervision
 
-  validates_presence_of :user, :supervision, :contact
+  validates_presence_of :user, :supervision, :content
 
   attr_accessible :content
 end
