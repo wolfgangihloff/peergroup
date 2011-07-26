b4aed270861b729f35d597c661acef70605256dc
Wojciech Wnętrzak <w.wnetrzak@objectreload.com>
2011-07-26 15:56:17 +0200

    removed unused path helpers

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -3,4 +3,16 @@ module GroupsHelper
     base = group.name
     user == group.founder ? "#{base}*" : base
   end
+
+  def link_to_group_supervision(text, group)
+    if current_supervision = group.supervisions.current
+      if current_supervision.members.include?(current_user)
+        link_to text, supervision_path(current_supervision)
+      else
+        link_to text, supervision_membership_path(current_supervision), :method => :post
+      end
+    else
+      link_to text, new_group_supervision_path(group)
+    end
+  end
 end
