19010f1f497d14859d429321a29d8b2e8b121a42
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-07-27 10:38:30 +0200

    display chat window on group page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_filter :authenticate
-
+  before_filter :fetch_chat, :only => [:show]
   def index
     @title = t(".title.your_groups", :default => "Your Groups")
     @groups = current_user.groups
@@ -41,7 +41,15 @@ class GroupsController < ApplicationController
 
   def show
     @title = t(".title", :default => "Group overview")
+  end
+
+  protected
+
+  def fetch_chat
     @group = Group.find(params[:id])
+    @chat_room ||= @group.chat_room
+    @chat_messages = @chat_room.chat_messages.recent
+    @token = @chat_room.set_redis_access_token_for_user(current_user)
   end
 
 end
