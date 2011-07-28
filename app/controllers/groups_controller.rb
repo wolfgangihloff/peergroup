75a82153cb449633a2730e135cba9329f6621ac0
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-07-28 11:08:25 +0200

    reorganized groups page + filter groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_filter :authenticate
   before_filter :fetch_chat, :only => [:show]
+  before_filter :filters, :only => [:all]
   def index
     @title = t(".title.your_groups", :default => "Your Groups")
     @groups = current_user.groups
@@ -8,7 +9,8 @@ class GroupsController < ApplicationController
 
   def all
     @title = t(".title.all_groups", :default => "All Groups")
-    @groups = Group.all
+    @user_groups = current_user.groups
+    @groups = Group.order("#{sort_method} #{sort_direction}").paginate(:page => params[:page], :per_page => 10)
     render :index
   end
 
@@ -23,5 +25,18 @@ class GroupsController < ApplicationController
     @chat_room ||= @group.chat_room
     @chat_messages = @chat_room.chat_messages.recent
     @token = @chat_room.set_redis_access_token_for_user(current_user)
+  end
+
+  def filters
+    @filters ||= ["date", "membership", "name"]
+  end
+
+  def sort_method
+    @methods = {:membership => "(SELECT COUNT(group_id) FROM memberships WHERE group_id = groups.id)", :name => "name"}
+    @methods.has_key?(params[:filter].try(:to_sym)) ? @methods[params[:filter].to_sym] : "created_at"
+  end
+
+  def sort_direction
+    params[:direction].try(:upcase) == "ASC" ? "ASC" : "DESC"
   end
 end
