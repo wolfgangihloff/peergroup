df4c2ed31de9289dc1d28fe43e4f78ae5f22d78c
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-08-10 09:57:34 +0200

    added tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,10 +7,18 @@ feature "Users" do
     sign_in_interactive(@user)
   end
 
-  scenario "Listing users" do
-    FactoryGirl.create(:user, :name => "Billy")
-    visit "/users"
-    page.should have_content("John")
-    page.should have_content("Billy")
+  scenario "In public profile page email should be visible only if user checked show_email field" do
+    @billy = FactoryGirl.create(:user, :name => "Billy", :show_email => true, :email => "billy@example.com")
+    @bob = FactoryGirl.create(:user, :name => "Billy", :show_email => false, :email => "bob@example.com")
+    visit user_path(@billy)
+    page.should have_content "billy@example.com"
+    visit user_path(@bob)
+    page.should_not have_content "bob@example.com"
+  end
+
+  scenario "Should display private profile page for current user" do
+    visit user_path(@user)
+    page.should have_content "Hi John"
+    page.should have_content "Change Your gravatar image"
   end
 end
