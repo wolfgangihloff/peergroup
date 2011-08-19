af0b4e73318e9913649cabad16384a5a7c4922d5
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-08-19 11:24:39 +0200

    code refactorization

--- app/javascripts/components/countdown_timer.js
+++ app/javascripts/components/countdown_timer.js
@@ -1,59 +1,67 @@
 (function ($) {
-  var seconds, element, interval,
-      _options = {},
-      row = 0,
-      col = 0;
-  $.defaults = {
-    rows: 4, 
-    cols: 7,
-    time: 1000,
-    gridSize: 60,
-    stopAtEnd: true,
-    displayTime: true,
-    displaySeconds: 35,
-    onStart: function () {},
-    onStop:  function () {}
-  },
 
-  $.tick = function () {
-    if (row > _options.rows ) {
-      row = 0;
-      col += 1;
-    }
-    if (col > _options.cols || seconds === 0) {
-      if (_options.stopAtEnd) {
-        $.stop();
-      }
-      col = 0;
-      seconds = _options.displaySeconds;
-    }
-    verticalPosition = col * _options.gridSize * -1;
-    horizontalPosition = row * _options.gridSize * -1;
-    position = horizontalPosition + "px " + verticalPosition + "px";
-    $(element).css("background-position", position);
-    if (_options.displayTime) {
-      $(element).text(--seconds);
-    }
-    ++row;
-  };
+    $.fn.countdownTimer = function (options) {
+        options = $.extend({
+            rows: 4,
+            cols: 7,
+            time: 1700,
+            gridSize: 60,
+            stopAtEnd: true,
+            displayTime: false,
+            displaySeconds: 60,
+            onStart: function () {},
+            onStop: function () {}
+        }, options);
 
-  $.start = function () {
-    _options.onStart();
-    interval = setInterval(function(){ $.tick(_options) }, _options.time);
-  };
+        return $(this).each(function (i, element) {
+            var seconds, interval
+                row = 0,
+                col = 0;
 
-  $.stop = function () {
-    _options.onStop();
-    clearInterval(interval);
-  }
+            seconds = options.displaySeconds;
+            start();
+            function tick() {
+                var verticalPosition, horizontalPosition, position;
 
-  $.fn.countdown_timer = function (options) {
-    element = this;
-    $.extend (_options, $.defaults, options);
-    seconds = _options.displaySeconds;
-    if ( _options.displayTime) {
-      $(element).html(seconds);
-    }
-    return $;
-  };
+                if (row > options.rows) {
+                    row = 0;
+                    col += 1;
+                }
+
+                if (col > options.cols || seconds === 0) {
+                    if (options.stopAtEnd) {
+                        stop();
+                    }
+                    col = 0;
+                    seconds = options.displaySeconds;
+                }
+                verticalPosition = col * options.gridSize * -1;
+                horizontalPosition = row * options.gridSize * -1;
+                position = horizontalPosition + "px " + verticalPosition + "px";
+                $(element).css("background-position", position);
+
+                if (options.displayTime) {
+                    $(element).text(--seconds);
+                }
+                ++row;
+            };
+
+            function start() {
+                options.onStart();
+                interval = setInterval(function () {
+                    tick(options);
+                }, options.time);
+            };
+
+            function stop() {
+                options.onStop();
+                clearInterval(interval);
+            };
+
+            if (options.displayTime) {
+                $(element).html(seconds);
+            }
+        });
+    };
+
 })(jQuery);
