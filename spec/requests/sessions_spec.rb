93705ed907756c622ad2e19827563213318948e8
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-09-30 10:30:52 +0200

    signup, user page, require password only once

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -6,7 +6,6 @@ feature "Session" do
     fill_in "Name", :with => "John"
     fill_in "Email", :with => "john@doe.com"
     fill_in "Password", :with => "secret"
-    fill_in "Password confirmation", :with => "secret"
     fill_in "Passcode", :with => "Pat0ng0"
     click_button "Sign up"
     User.exists?(:name => "John").should be_true
